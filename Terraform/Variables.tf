9b2aeeb6051749c2c2c00660afda9b330e52cd5c
monikakostovska <monicca@live.com>
2023-04-11 23:50:20 +0200

    Terraform labs

---
+++ Terraform/Variables.tf
@@ -0,0 +1,11 @@
+variable "my_name" {
+  type = string
+  description = "First name of the student"
+}
+
+variable "location" {
+  type = string
+  default = "West Europe"
+  description = "The location where all resources will be placed."
+}
+
